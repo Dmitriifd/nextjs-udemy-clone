3158721ab352117a71e601aa8ae98cf57c054024
Dmitriifd <idimka9@gmail.com>
2023-11-23 20:06:25 +0500

    Course Category Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -8,6 +8,7 @@ import { IconBadge } from '@/components/icon-badge';
 import { TitleForm } from './_components/title-form';
 import { DescriptionForm } from './_components/description-form';
 import { ImageForm } from './_components/image-form';
+import { CategoryForm } from './_components/category-form';
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -32,6 +33,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           createdAt: 'desc',
         },
       },
+    },
+  });
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: 'asc',
     },
   });
 
@@ -73,6 +80,15 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
             <TitleForm initialData={course} courseId={course.id} />
             <DescriptionForm initialData={course} courseId={course.id} />
             <ImageForm initialData={course} courseId={course.id} />
+
+            <CategoryForm
+              initialData={course}
+              courseId={course.id}
+              options={categories.map((category) => ({
+                label: category.name,
+                value: category.id,
+              }))}
+            />
           </div>
         </div>
       </div>
